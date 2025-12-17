be92b1df8a8a00dab256b0ace8e2407b6ab22d6c
erjagdeep2307 <erjagdeep2307@gmail.com>
2025-12-17 13:05:29 +0530

    Logo Changed and Images optimized

--- src/client/src/components/Footer/Footer.tsx
+++ src/client/src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import logo from "/logo-54.png";
+import logo from "/logo.svg";
 import { MapPin, Phone, Mail } from "lucide-react"
 export default function Footer(){
     return (
