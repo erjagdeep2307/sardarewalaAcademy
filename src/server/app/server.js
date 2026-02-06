ca0d4cdf537f8ea2743d14fb0072d351a9a19a21
erjagdeep2307 <erjagdeep2307@gmail.com>
2026-02-06 12:41:23 +0530

    Added Uncaught Exception Handling in server.js and also removed unused logger from server file

--- src/server/app/server.js
+++ src/server/app/server.js
@@ -1,11 +1,16 @@
 import 'dotenv/config';
 import app from './adaptors/http-express/app.js';
 import { connectDB } from '#db';
-import logger from '#logger';
-import requestLogger from './adaptors/middleware/logMiddleware.js';
 const PORT = process.env.APP_PORT || 5935;
 // Check out the initial connection to Database if it is able to make connection or not
 await connectDB();
 app.listen(PORT, () => {
   logger.info(`Server is running on port ${PORT}`);
 }); 
+
+// Applied Global Exception Handler in case of Any Uncaught Exception
+
+process.on('uncaughtException',(err)=>{
+    console.error(`Got an Exception :${err.message}`);
+    exit(1);
+})
