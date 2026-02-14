a18ab87863aa6af77b907098ffd52e8c6d6951d3
erjagdeep2307 <erjagdeep2307@gmail.com>
2026-02-14 14:46:10 +0530

    Change BASE_URL environment variable reference
    for testing

--- src/client/src/apis/httpClient.ts
+++ src/client/src/apis/httpClient.ts
@@ -5,7 +5,7 @@ type HttpClientOptions = Omit<RequestInit, "body"> & {
   body?: validBody;
 };
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
+const BASE_URL = import.meta.env.API_BASE_URL as string;
 
 const httpclient = async <T>(endpoint: string, options: HttpClientOptions = {}): Promise<T> => {
   const { body,...restOptions } = options;
